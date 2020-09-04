f7a0749ab76fc397904583f4768eba96f983073e
Patryk Zawadzki <patrys@room-303.com>
2020-09-04 12:43:31 +0200

    1.1.0
    Updated supported Python versions

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ CLASSIFIERS = [
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
@@ -19,7 +20,7 @@ setup(
     description=(
         'A Python implementation of Google Analytics Measurement Protocol'),
     license='BSD',
-    version='1.0.0',
+    version='1.1.0',
     packages=['google_measurement_protocol'],
     install_requires=['requests>=2.0,<3.0a0', 'prices>=1.0.0', 'typing>=3.6.0;python_version<"3.5"'],
     classifiers=CLASSIFIERS,
